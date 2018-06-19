b013a21295032988f9f742464e341b8bc7365d68
Brent Bauman <brentbauman86@gmail.com>
2018-06-19 14:17:55 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -56,9 +56,7 @@ module Players
         end
       elsif board.turn_count == 7
         if board.taken?(1) && board.taken?(7)
-          "4"
-        unless taken?
-        end 
+          "4" 
         elsif board.taken?(1) && board.taken?(3)
           "2"
         elsif board.taken?(7) && board.taken?(9)
