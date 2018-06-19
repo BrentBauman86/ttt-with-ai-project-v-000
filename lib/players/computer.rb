d13aacf8612e0c1efbb953c647d26bc67f182830
Brent Bauman <brentbauman86@gmail.com>
2018-06-19 14:05:33 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -15,77 +15,61 @@ module Players
         "9" unless board.taken?(9)
         corners(board)
       elsif board.turn_count == 3
-  #       if board.taken?(5)
-  #         if board.taken?(1)
-  #           "9"
-  #         elsif board.taken?(2)
-  #           "8"
-  #         elsif board.taken?(3)
-  #           "7"
-  #         elsif board.taken?(4)
-  #           "6"
-  #       end
-  #     else (rand(9) + 1).to_s
-  #     end
-  #   end
-  # end
-edges(board)
-elsif board.turn_count == 4
-  if board.taken?(2) && board.taken?(5)
-    "8"
-  elsif board.taken?(8) && board.taken?(5)
-    "2"
-  elsif board.taken?(4) && board.taken?(5)
-    "6"
-  elsif board.taken?(6) && board.taken?(5)
-    "4"
-  elsif board.taken?(3) && board.taken?(5)
-    "7"
-  else
-    (rand(9) + 1).to_s
-  end
-  elsif board.turn_count == 5
-    if board.taken?(2) && board.taken?(1)
-      "3"
-    elsif board.taken?(4) && board.taken?(1)
-      "7"
-    elsif board.taken?(6) && board.taken?(9)
-      "3"
-    elsif board.taken?(8) && board.taken?(9)
-      "7"
-    else
-      (rand(9) + 1).to_s
-    end
-
-  elsif board.turn_count == 6
-    if board.taken?(3) && board.taken?(5)
-      "7"
-    elsif board.taken?(7) && board.taken?(5)
-      "3"
-    elsif board.taken?(6) && board.taken?(5)
-      "4"
-    elsif board.taken?(4) && board.taken?(5)
-      "6"
-    else
-      (rand(9) + 1).to_s
-    end
-
-    elsif board.turn_count == 7
-      if board.taken?(1) && board.taken?(7)
-        "4"
-      elsif board.taken?(1) && board.taken?(3)
-        "2"
-      elsif board.taken?(7) && board.taken?(9)
-        "8"
-      elsif board.taken?(9) && board.taken?(3)
-        "6"
+        edges(board)
+      elsif board.turn_count == 4
+        if board.taken?(2) && board.taken?(5)
+          "8"
+        elsif board.taken?(8) && board.taken?(5)
+          "2"
+        elsif board.taken?(4) && board.taken?(5)
+          "6"
+        elsif board.taken?(6) && board.taken?(5)
+          "4"
+        elsif board.taken?(3) && board.taken?(5)
+          "7"
+        else
+          (rand(9) + 1).to_s
+        end
+      elsif board.turn_count == 5
+        if board.taken?(2) && board.taken?(1)
+          "3"
+        elsif board.taken?(4) && board.taken?(1)
+          "7"
+        elsif board.taken?(6) && board.taken?(9)
+          "3"
+        elsif board.taken?(8) && board.taken?(9)
+          "7"
+        else
+          (rand(9) + 1).to_s
+        end
+      elsif board.turn_count == 6
+        if board.taken?(3) && board.taken?(5)
+          "7"
+        elsif board.taken?(7) && board.taken?(5)
+          "3"
+        elsif board.taken?(6) && board.taken?(5)
+          "4"
+        elsif board.taken?(4) && board.taken?(5)
+          "6"
+        else
+          (rand(9) + 1).to_s
+        end
+      elsif board.turn_count == 7
+        if board.taken?(1) && board.taken?(7)
+          "4"
+        elsif board.taken?(1) && board.taken?(3)
+          "2"
+        elsif board.taken?(7) && board.taken?(9)
+          "8"
+        elsif board.taken?(9) && board.taken?(3)
+          "6"
+        else
+          (rand(9) + 1).to_s
+        end
       else
         (rand(9) + 1).to_s
       end
-    else
-      (rand(9) + 1).to_s
     end
-  end
 
     def corners(board)
       "1" unless board.taken?(1)
@@ -101,7 +85,8 @@ elsif board.turn_count == 4
       "8" unless board.taken?(8)
     end
 
-      end
-    end
+  end
+end
 #   end
 # end
+
