2f7a9c53bb34f38b696affdc7d8616817de20d2d
Brent Bauman <brentbauman86@gmail.com>
2018-06-19 14:16:02 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -57,6 +57,7 @@ module Players
       elsif board.turn_count == 7
         if board.taken?(1) && board.taken?(7)
           "4"
+          unless taken?
         elsif board.taken?(1) && board.taken?(3)
           "2"
         elsif board.taken?(7) && board.taken?(9)
@@ -87,4 +88,3 @@ module Players
 
   end
 end
-
